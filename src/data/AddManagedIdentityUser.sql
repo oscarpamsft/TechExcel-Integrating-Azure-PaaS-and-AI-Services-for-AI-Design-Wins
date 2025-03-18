503321113fee38f873681b917d4969f262d72045
Oscar Palomo <osc.palomo@gmail.com>
2025-03-18 22:23:24 +0000

    Task 02 - Prepare an Azure SQL Database

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,3 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
-
+CREATE USER id-ezqq6q4qvd3po FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-ezqq6q4qvd3po;
+ALTER ROLE db_datawriter ADD MEMBER id-ezqq6q4qvd3po;

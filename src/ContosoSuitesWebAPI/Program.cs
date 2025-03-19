1d5ada5fb8c9ee6dc9a8cecb1ca852e0d1ef310d
Oscar Palomo <osc.palomo@gmail.com>
2025-03-19 13:40:42 +0000

    E05 - Task 02 - Create a Semantic Kernel Agent

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -63,17 +63,32 @@ builder.Services.AddSingleton<Kernel>((_) =>
         endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
         apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
     );
-
-   var databaseService = _.GetRequiredService<IDatabaseService>();
+    var databaseService = _.GetRequiredService<IDatabaseService>();
     kernelBuilder.Plugins.AddFromObject(databaseService);
 
-#pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+    #pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
     kernelBuilder.AddAzureOpenAITextEmbeddingGeneration(
      deploymentName: builder.Configuration["AzureOpenAI:EmbeddingDeploymentName"]!,
      endpoint: builder.Configuration["AzureOpenAI:Endpoint"]!,
      apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
      );
+    #pragma warning restore SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
+    kernelBuilder.Plugins.AddFromType<MaintenanceRequestPlugin>("MaintenanceCopilot");
+    kernelBuilder.Services.AddSingleton<CosmosClient>((_) =>
+      {
+          string userAssignedClientId = builder.Configuration["AZURE_CLIENT_ID"]!;
+          var credential = new DefaultAzureCredential(
+              new DefaultAzureCredentialOptions
+              {
+                  ManagedIdentityClientId = userAssignedClientId
+              });
+          CosmosClient client = new(
+              accountEndpoint: builder.Configuration["CosmosDB:AccountEndpoint"]!,
+              tokenCredential: credential
+          );
+          return client;
+      });
     return kernelBuilder.Build();
 });
 
@@ -175,7 +190,10 @@ app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromService
 app.MapPost("/MaintenanceCopilotChat", async ([FromBody]string message, [FromServices] MaintenanceCopilot copilot) =>
 {
     // Exercise 5 Task 2 TODO #10: Insert code to call the Chat function on the MaintenanceCopilot. Don't forget to remove the NotImplementedException.
-    throw new NotImplementedException();
+    //throw new NotImplementedException();
+    var response = await copilot.Chat(message);
+    return response;
+
 })
     .WithName("Copilot")
     .WithOpenApi();
